9443d24f166e840a0aa51d10cba59fb579e2c14e
Ioannis Kalogeropoulos <36636427+johnkalog@users.noreply.github.com>
2019-08-11 13:57:12 +0300

    Add files via upload

---
+++ bid-app-backend/src/test/java/com/bid/Bid/BidApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bid.Bid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BidApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
