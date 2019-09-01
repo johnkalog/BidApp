65b48d40567255b677be8e21fa73956e22e73cb2
johnkalog <JohnKal97@gmail.com>
2019-09-01 15:46:01 +0300

    pop-m

--- bid-app-frontend/src/components/SpecificMessagePop.js
+++ bid-app-frontend/src/components/SpecificMessagePop.js
@@ -2,28 +2,45 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { changeShow } from '../actions/messageActions';
 
-const SpecificMessagePop = ({ changeShow }) => {
+const SpecificMessagePop = ({ message, inboxOrNot, changeShow }) => {
+  const first = inboxOrNot ? 'From' : 'To';
   return (
     <div>
       <div className="popup">
-        <div className="popup_inner"></div>
-        <button
-          type="button"
-          className="close-message"
-          aria-label="Close"
-          onClick={() => {
-            changeShow();
-          }}
-        >
-          <span aria-hidden="true">&times;</span>
-        </button>
+        <div className="popup_inner">
+          <h4>{'Details'}</h4>
+          <ul className="poplist">
+            <li>
+              <p>{`${first}: ${message.subject.split('-')[0]}`}</p>
+            </li>
+            <li>
+              <p>{`Subject: ${message.subject}`}</p>
+            </li>
+            <li>
+              <p>{`Message: ${message.message}`}</p>
+            </li>
+          </ul>
+          <button
+            type="button"
+            className="close"
+            aria-label="Close"
+            onClick={() => {
+              changeShow();
+            }}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default connect(
-  null,
+  state => ({
+    message: state.messagesData.message,
+    inboxOrNot: state.messagesData.inboxOrNot
+  }),
   dispatch => ({
     changeShow: changeShow(dispatch)
   })
